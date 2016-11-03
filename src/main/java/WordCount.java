be7a275ed373af01ccc4353e5c1cc812f9801625
Tianming Zhuang <tianming.zhuang@gmail.com>
2016-11-03 12:44:11 -0400

    Passing tests 1-3

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,2 +1,22 @@
+import java.util.*;
+
 public class WordCount {
+  private Map<String, Integer> wordCounts;
+
+  public WordCount() {
+  }
+
+  public Map<String, Integer> phrase(String sentence) {
+    this.wordCounts = new HashMap<String, Integer>();
+    String[] sentenceArray = sentence.split(" ");
+    for (String word : sentenceArray) {
+      if (wordCounts.get(word) == null) {
+        wordCounts.put(word, 1);
+      } else {
+        wordCounts.put(word, wordCounts.get(word) + 1);
+      }
+    }
+
+    return this.wordCounts;
+  }
 }
